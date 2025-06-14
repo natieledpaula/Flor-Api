facaf75095892cbd96b6114ea3ba9aa8fba9d201
natieledpaula <natiele887@gmail.com>
2025-06-14 15:30:46 -0300

    validação de cep incontrado ou cep incorreto

--- src/js/cadastro.js
+++ src/js/cadastro.js
@@ -5,6 +5,23 @@ const eNumero = (numero) => /^[0-9]+$/.test(numero);
 const cepValido = (cep) => cep.length === 8 && eNumero(cep);
 
 // Busca o CEP
-const buscaCep = async (cep) => {
-    
+const buscaCep = async () => {
+    limparFormulario();
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`;
+
+    if(cepValido(cep.value)) {
+        const dados = await fetch(url);
+        const address = await dados.json();
+
+        // hasOWnProperty rertorna um valor booleano indicado se o objetivo possui a propriedade especifica no parenteses
+        if(address.hasOwnProperty('erro')) {
+            alert("CEP não encontrado!");
+        } else {
+            preencherFormulario(address);
+        }
+    } else {
+        alert("CEP incorreto, tente novamente!");
+    }
 }
+
+// Preencher formulario
